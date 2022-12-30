a7957d7a6c4c979c9d5dcbfae857a8030e3ac2e9
Muhammad-Umair1049 <2021CS144@student.uet.edu.pk>
2022-12-31 01:32:30 +0500

    .

--- DsaFinalProject/Program.cs
+++ DsaFinalProject/Program.cs
@@ -16,7 +16,7 @@ namespace DsaFinalProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Opening());
+            Application.Run(new PickUp());
         }
     }
 }
--- DsaFinalProject/Resources/PickUp.Designer.cs
+++ DsaFinalProject/Resources/PickUp.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DsaFinalProject.Properties.Resources.circle_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(480, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // PickUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1042, 542);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "PickUp";
             this.Text = "PickUp";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
